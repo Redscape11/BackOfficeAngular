f93d70fb70a65222fd865cb75f441d54913df915
emnak97 <emnakousaier33@gmail.com>
2022-01-21 13:11:13 +0100

    Sales and quantity charts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -12,19 +12,44 @@ import {ListproductsComponent } from 'app/pages/list-products/list-products.comp
 })
 export class DashboardComponent implements OnInit {
 
+    emailChartType: ChartType;
+    emailChartData: any;
+    emailChartLegendItems: LegendItem[];
+	
+    hoursChartType: ChartType;
+    hoursChartOptions: any;
+    hoursChartResponsive: any[];
+    hoursChartLegendItems: LegendItem[];
+	
+	activityChartType: ChartType;
+    activityChartData: any;
+    activityChartOptions: any;
+    activityChartResponsive: any[];
+    activityChartLegendItems: LegendItem[];
     
-    public hoursChartType: ChartType;
-    public hoursChartOptions: any;
-    public hoursChartResponsive: any[];
-    public hoursChartLegendItems: LegendItem[];
+    PoissonsQuantity;
+    CrustacesQuantity;
+    CoquillagesQuantity;
+    products;
+    categories = [
+      { "id": 1, "category": 0,"name":"poissons", "products": null  },
+      { "id": 2, "category": 1,"name":"coquillages", "products": null },
+      { "id": 3, "category": 2, "name":"crustaces","products": null },
+    ];
 
     public transactions :any;
-
   constructor(public productsService: ProductsService) {
 
   }
 
   ngOnInit() {
+	  
+	  this.emailChartType = ChartType.Pie;
+      this.emailChartLegendItems = [
+        { title: 'Poissons', imageClass: 'fa fa-circle text-info' },
+        { title: 'Coquillages', imageClass: 'fa fa-circle text-warning' },
+        { title: 'Crustacés', imageClass: 'fa fa-circle text-danger' }
+      ];
      
       this.hoursChartType = ChartType.Line;
       this.hoursChartOptions = {
@@ -57,7 +82,33 @@ export class DashboardComponent implements OnInit {
       ];
      
       
-    
+	  this.activityChartType = ChartType.Bar;
+      this.activityChartOptions = {
+        seriesBarDistance: 10,
+        axisX: {
+          showGrid: false
+        },
+        height: '245px'
+      };
+      this.activityChartResponsive = [
+        ['screen and (max-width: 640px)', {
+          seriesBarDistance: 5,
+          axisX: {
+            labelInterpolationFnc: function (value) {
+              return value[0];
+            }
+          }
+        }]
+      ];
+      this.activityChartLegendItems = [
+        { title: 'Poissons', imageClass: 'fa fa-circle text-info' },
+        { title: 'Coquillages', imageClass: 'fa fa-circle text-danger' },
+        { title: 'Crustaces', imageClass: 'fa fa-circle text-warning' }
+      ];
+      this.getProducts();
+      this.getTransaction();
+      this.getTotalQuantityByCategory();
+      this.getTotalSalesByCategory();
       this.getTransaction();
     }
     getTransaction() {
@@ -121,6 +172,75 @@ export class DashboardComponent implements OnInit {
         labels : labels,
         series : [CA,Marge,Cout]
       }
+	  getProductsAll() {
+      for (let i = 0; i < this.categories.length; i++){
+        this.getProductsCategory(this.categories[i].category);
+      }
+    }
+  
+    getProductsCategory(cat) {
+      this.productsService.getProductCategories(cat).subscribe(res => {
+        for (let i = 0; i < this.categories.length; i++)
+          if (this.categories[i].category == cat) {
+            this.categories[i].products = res;
+          }
+      },
+        (err) => {
+          alert('failed loading json data');
+        });
+    }
+    getProducts() {
+      this.productsService.getProducts().subscribe(res => {
+        this.products = res;
+      },
+        (err) => {
+          alert('failed loading json data');
+        });
+    }
+    getTotalQuantityByCategory(){
+      let res =[]
+      let poissonQuantity=0
+      let coquillageQuantity=0
+      let crustacesQuantity=0
+      for (let i = 0; i < this.products.length; i++) 
+        if (this.products[i].category ==0)
+          poissonQuantity+=this.products[i].quantityInStock;
+        else if (this.products[i].category ==1)
+          coquillageQuantity+=this.products[i].quantityInStock;
+        else
+        crustacesQuantity+=this.products[i].quantityInStock;
+      let total = poissonQuantity+coquillageQuantity+crustacesQuantity
+      let poisson = ((poissonQuantity/total)*100).toFixed(2);
+      let coquillage = ((coquillageQuantity/total)*100).toFixed(2)
+      let crustaces = ((crustacesQuantity/total)*100).toFixed(2)
+      
+      return {
+        labels :[poisson.toString()+'%',coquillage.toString()+'%',crustaces.toString()+'%'],
+        series :[poisson,coquillage,crustaces]
+      }
+    }
+    getTotalSalesByCategory(){
+      let res =[]
+      let poissonSales=0
+      let coquillagesSales=0
+      let crustacesSales=0
+      for (let i = 0; i < this.products.length; i++) 
+        if (this.products[i].category ==0)
+          poissonSales+=this.products[i].nombre_produit_vendu;
+        else if (this.products[i].category ==1)
+          coquillagesSales+=this.products[i].nombre_produit_vendu;
+        else
+        crustacesSales+=this.products[i].nombre_produit_vendu;
+      let total = poissonSales+coquillagesSales+crustacesSales
+      let poisson = ((poissonSales/total)*100).toFixed(2);
+      let coquillage = ((coquillagesSales/total)*100).toFixed(2)
+      let crustaces = ((crustacesSales/total)*100).toFixed(2)
+      
+      return {
+        labels :['Nombre de produits vendus'],
+        series :[[poissonSales],[coquillagesSales],[crustacesSales]]
+      }
+    }
     }
  
 }
